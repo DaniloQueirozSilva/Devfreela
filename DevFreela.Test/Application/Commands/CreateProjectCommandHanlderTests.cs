4dbc8936a2ea63f504a25bfb5f85b4a02f0839e8
Danilo Queiroz <47929020+DaniloQueirozSilva@users.noreply.github.com>
2024-12-08 20:08:46 -0300

    Criação de testes unitários

---
+++ DevFreela.Test/Application/Commands/CreateProjectCommandHanlderTests.cs
@@ -0,0 +1,44 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Test.Application.Commands
+{
+   
+    public class CreateProjectCommandHanlderTests
+    {
+        [Fact]
+        public async Task InputDataIsOk_Executed_ReturnProjectId()
+        {
+            //Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Test",
+                Description = "Test",
+                TotalCost = 1000,
+                IdClient = 1,
+                IdFreelancer = 2,
+
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepository.Object);
+
+            // Act
+
+            var id = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());            
+
+            //Asset
+
+            Assert.True(id >= 0);
+            projectRepository.Verify(pr => pr.AddAsync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
---
+++ DevFreela.Test/Application/Queries/GetAllProjectsCommandHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.Test.Application.Queries
+{
+    public class GetAllProjectsCommandHandlerTests
+    {
+        [Fact]
+        public async Task ThreeProjectsExist_Executed_ReturnThreeProjectViewModels() 
+        {
+            //Arrange
+            var projects = new List<Project>
+            {
+                new Project("Nome do Teste 1", "Descricao do teste 1",1,2,10000),
+                new Project("Nome do Teste 2", "Descricao do teste 2",1,2,20000),
+                new Project("Nome do Teste 3", "Descricao do teste 3",1,2,30000)
+            };
+
+            var projectRepository = new Mock<IProjectRepository>();
+            projectRepository.Setup(pr => pr.GetAllAsync().Result).Returns(projects);
+
+            var getAllProjects = new GetAllProjectsQuery("");
+            var getAllProjectsQueryHandler = new GetAllProjectsQueryHandler(projectRepository.Object);
+
+            //Act
+            var proectViewModelList = await getAllProjectsQueryHandler.Handle(getAllProjects, new CancellationToken());
+
+            //Assert
+
+            Assert.NotNull(proectViewModelList);
+            Assert.NotEmpty(proectViewModelList);
+            Assert.Equal(3, proectViewModelList.Count);
+
+
+            projectRepository.Verify(pr => pr.GetAllAsync().Result, Times.Once);
+        }
+    }
+}
