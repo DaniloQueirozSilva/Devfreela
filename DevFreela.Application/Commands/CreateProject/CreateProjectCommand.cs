95f79d0045a4d9a5d0d424a31cc89d266d8927b2
Danilo Queiroz <47929020+DaniloQueirozSilva@users.noreply.github.com>
2025-03-01 04:09:44 -0300

    Update CreateProjectCommand.cs

--- DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
+++ DevFreela.Application/Commands/CreateProject/CreateProjectCommand.cs
@@ -5,7 +5,7 @@ namespace DevFreela.Application.Commands.CreateProject
 {
     public class CreateProjectCommand : IRequest<int>
     {
-        public strin Title { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public int IdClient { get; set; }
         public int IdFreelancer { get; set; }
